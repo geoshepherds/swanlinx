de6fc6b8819b3cf64f391ab86d05753714e7497c
owain-stratton <owain@geoshepherds.com>
2016-07-04 22:28:14 +0200

    added map elements and responsive scaling when window resizes, all data added and ready to be styled

--- scripts/app.js
+++ scripts/app.js
@@ -35,31 +35,12 @@ $(document).ready(function() {
     
     // D3 MAP AND VIZ
     
-//    d3.select(window).on('resize', resize)
-//    
-//    function resize() {
-//	    d3.select("g").attr("transform", "scale(" + $("#container").width()/900 + ")");
-//	    $('svg').height($('#map').width()*0.618);
-//	}
-    
-    
-//    var map = $('#svg'),
-//        aspect = map.width() / map.height(),
-//        container = map.parent();
-//    $(window).on("resize", function() {
-//        var targetWidth = container.width();
-//        map.attr("width", targetWidth);
-//        map.attr("height", Math.round(targetWidth / aspect));
-//    }).trigger("resize");
-//    
-    
-    
-    var width = 1920,
-        height = 1080;
+    var width = $('#map').width(),
+        height = $('#map').height();
     
     var projection = d3.geo.mercator()
         .center([-3.994, 51.65])
-        .scale(55000)
+        .scale(width * 110)
         .translate([width / 2, height / 2]);
     
     var geoPath = d3.geo.path()
@@ -68,8 +49,6 @@ $(document).ready(function() {
     var svg = d3.select('#map').append('svg')
         .attr({
             id: 'svg',
-//            viewBox: '0 0 ' + width + ' ' + height,
-//            preserveAspectRatio: 'xMidYMid meet',
             width: width,
             height: height
         })
@@ -87,17 +66,19 @@ $(document).ready(function() {
     var q = d3.queue()
         .defer(d3.json, '/data/hex_grid.json')
         .defer(d3.json, '/data/ua_regions.json')
+        .defer(d3.json, '/data/swanlinx-data.json')
         .defer(d3.json, '/data/pop_places.geojson')
         .await(makeMap);
     
     
-    function makeMap(error, hexTopo, regTopo, places) {
+    function makeMap(error, hexTopo, regTopo, dataTopo, places) {
         
         if (error) throw error;
         
-        
         var hexGrid = topojson.feature(hexTopo, hexTopo.objects.hex_grid);
         var regions = topojson.feature(regTopo, regTopo.objects.ua_regions);
+        var data = topojson.feature(dataTopo, dataTopo.objects['swanlinx-data']);
+        
         
         // SOUTH WALES UNITARY AUTHORITY BOUNDARIES
         
@@ -106,9 +87,9 @@ $(document).ready(function() {
                 id: 'uaGroup'
             });
         
-        uaGroup.selectAll('path.ua')
-            .data(regions.features)
-            .enter().append('path')
+        uaGroup
+            .append('path')
+            .datum(regions)
             .attr({
                 class: 'ua',
                 d: geoPath
@@ -125,10 +106,10 @@ $(document).ready(function() {
             .attr({
                 id: 'hexGroup'
             });
-            
-        hexGroup.selectAll('path.hex')
-            .data(hexGrid.features)
-            .enter().append('path')
+        
+        hexGroup
+            .append('path')
+            .datum(hexGrid)
             .attr({
                 class: 'hex',
                 d: geoPath
@@ -189,6 +170,65 @@ $(document).ready(function() {
             .style('text-anchor', function(d) { 
                 return d.geometry.coordinates[0] > -3.9 ? 'start' : 'end'; 
             });
-    }
     
-});
+    
+        // MAP SWANLINX DATA
+        
+        var dataGroup = g.append('g')
+            .attr({
+                id: 'dataGroup'
+            });
+    
+        dataGroup.selectAll('path.hexBin')
+            .data(data.features)
+            .enter().append('path')
+            .attr({
+                class: 'hexBin',
+                d: geoPath
+            })
+            .style({
+                fill: function(d) {
+                    return '#575'
+                },
+                opacity: .7
+            });
+    
+    } // end makeMap function
+    
+    
+    // RESIZE FUNCTION
+    
+    $(window).resize(function() {
+        var w = $('#map').width(),
+            h = $('#map').height();
+        
+        svg.attr('width', w);
+        svg.attr('height', h);
+        
+        projection
+            .translate([w / 2, h / 2])
+            .scale(w * 110);
+        
+        d3.select('.ua').attr('d', geoPath);
+        d3.select('.hex').attr('d', geoPath);
+        d3.selectAll('.hexBin').attr('d', geoPath);
+        d3.selectAll('.place')
+            .attr({
+                transform: function(d) {
+                    return 'translate(' + projection([
+                        d.geometry.coordinates[0],
+                        d.geometry.coordinates[1]
+                    ]) + ')'
+                }
+            });
+        d3.selectAll('.place-label')
+            .attr({
+                transform: function(d) { 
+                    return "translate(" + projection(d.geometry.coordinates) + ")"; 
+                }
+            });
+        
+    }); // end resize function
+    
+    
+}); // end document ready function
