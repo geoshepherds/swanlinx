2dddba7ffd20841c8e9a37cf2c6f22a24f9372b0
owain-stratton <owain@geoshepherds.com>
2016-07-07 20:28:01 +0200

    changes to main css to add vendor prefixes

--- scripts/app.js
+++ scripts/app.js
@@ -572,5 +572,8 @@ $(document).ready(function() {
         
     }); // end resize function
     
+    setTimeout(function () {
+        $(window).scrollTo(0, 1);
+    }, 1000);
     
 }); // end document ready function
